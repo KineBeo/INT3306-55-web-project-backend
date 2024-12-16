318603afe460a21b1a368242546973ffaa989ab0
Lehoangminh-dab <hoangminh7c12@gmail.com>
2024-12-16 16:04:29 +0700

    fix: associated adult id no longer initiates to random value even though it is set to null

--- src/ticket-passenger/entities/ticket-passenger.entity.ts
+++ src/ticket-passenger/entities/ticket-passenger.entity.ts
@@ -1,3 +1,4 @@
+import { IsOptional } from 'class-validator';
 import { PassengerType } from 'src/enum/ticket-passenger/passenger_type';
 import { Ticket } from 'src/ticket/entities/ticket.entity';
 import {
@@ -20,10 +21,11 @@ export class TicketPassenger {
   })
   passenger_type: PassengerType;
 
+  @IsOptional()
   @ManyToOne(() => TicketPassenger, { nullable: true })
   @JoinColumn({ name: 'associated_adult_id' })
   associated_adult_id: TicketPassenger;
-
+  
   @ManyToOne(() => Ticket, (ticket) => ticket.ticketPassengers)
   @JoinColumn({ name: 'ticket_id' })
   ticket: Ticket;
